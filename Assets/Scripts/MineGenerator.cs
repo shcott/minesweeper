e5db1e80efe7a75abde81f58752ad834da49c41c
Scott <scott.huang.21@outlook.com>
2016-11-06 02:33:40 -0600

    Add initial code

---
+++ Assets/Scripts/MineGenerator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineGenerator : MonoBehaviour {
+
+	public int width;
+	public int height;
+
+	void Start () {
+		GenerateMines(.1f);
+	}
+
+	void GenerateMines(float mineLength) {
+		
+	}
+}

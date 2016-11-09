1e0233d5e1b5f76b4775e22f04b483d91dd06eae
Scott <scott.huang.21@outlook.com>
2016-11-09 03:15:28 -0600

    Add mine prefab; add initial input handling code

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour {
+
+	Camera cam;
+	MineGenerator mineGen;
+
+	void Start() {
+		cam = Camera.main;
+		mineGen = GetComponent<MineGenerator>();
+	}
+
+	void Update () {
+		bool leftClick = Input.GetButtonDown("Fire1");
+		bool rightClick = Input.GetButtonDown("Fire2");
+		if(leftClick || rightClick) {
+			RaycastHit hit;
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray, out hit)) {
+				GameObject obj = hit.transform.gameObject;
+				if(obj != null)
+					mineGen.HitMine(obj, leftClick, rightClick);
+			}
+		}
+	}
+}
--- Assets/Scripts/MineGenerator.cs
+++ Assets/Scripts/MineGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MineGenerator : MonoBehaviour {
 
@@ -7,31 +8,93 @@ public class MineGenerator : MonoBehaviour {
 	public int height;
 	public GameObject minePrefab;
 
-	Mine[,] mineMap;
+	Tile[,] tileMap;
+	Dictionary<GameObject, Coord> objToCoord;
 
-	void Start () {
-		mineMap = new Mine[width, height];
+	void Start() {
+		tileMap = new Tile[width, height];
+		objToCoord = new Dictionary<GameObject, Coord>();
 
-		GenerateMines(.1f);
+		GenerateTiles(.1f);
 	}
 
-	void GenerateMines(float mineLength) {
+	void GenerateTiles(float tileLength) {
 		for(int x = 0; x < width; x++) {
 			for(int y = 0; y < height; y++) {
-				Vector3 pos = new Vector3((-width/2f + .5f + x) * mineLength, (-height/2f + .5f + y) * mineLength, 0);
-				mineMap[x, y] = new Mine(minePrefab, pos, mineLength);
+				Vector3 pos = new Vector3((-width/2f + .5f + x) * tileLength, (-height/2f + .5f + y) * tileLength, 0);
+				tileMap[x, y] = new Tile(minePrefab, pos, tileLength);
+				objToCoord.Add(tileMap[x, y].tileObject, new Coord(x, y));
 			}
 		}
 	}
 
-	public class Mine {
-		public Vector3 position;
-		public GameObject mineObject;
+	public void HitMine(GameObject obj, bool leftClick, bool rightClick) {
+		Debug.Log(obj.name);
+		if(!objToCoord.ContainsKey(obj))
+			return;
+		Coord coord = objToCoord[obj];
+		Tile tile = tileMap[coord.x, coord.y];
+		if(rightClick) {
+			if(tile.tileState != TileState.CHECKED) {
+				tile.SetState(tile.tileState == TileState.DEFAULT ? TileState.FLAGGED : TileState.DEFAULT);
+			}
+		} else if(leftClick) {
+			if(tile.tileState != TileState.FLAGGED) {
+				tile.SetState(TileState.CHECKED);
+				if(tile.tileData.isMine) {
+					// Game over
+				}
+			}
+		}
+	}
 
-		public Mine(GameObject prefab, Vector3 pos, float scale) {
+	public struct Coord {
+		public int x;
+		public int y;
+
+		public Coord(int _x, int _y) {
+			x = _x;
+			y = _y;
+		}
+	}
+
+	public enum TileState {DEFAULT, CHECKED, FLAGGED};
+
+	public class Tile {
+		public Vector3 position;
+		public GameObject tileObject;
+		public TileState tileState;
+		public TileData tileData;
+
+		public Tile(GameObject prefab, Vector3 pos, float scale) {
 			position = pos;
-			mineObject = Instantiate(prefab, position, Quaternion.identity) as GameObject;
-			mineObject.transform.localScale = new Vector3(scale, scale, scale);
+			tileObject = Instantiate(prefab, position, Quaternion.identity) as GameObject;
+			tileObject.transform.localScale = new Vector3(scale, scale, scale);
+			tileState = TileState.DEFAULT;
+			tileData = new TileData();
+		}
+
+		public void SetState(TileState state) {
+			tileState = state;
+			foreach(Transform child in tileObject.transform) {
+				if(state == TileState.DEFAULT) {
+					child.gameObject.SetActive(child.name == "Default");
+				} else if(state == TileState.CHECKED) {
+					child.gameObject.SetActive(child.name == "Checked");
+				} else if(state == TileState.FLAGGED) {
+					child.gameObject.SetActive(child.name == "Flagged");
+				}
+			}
+		}
+	}
+
+	public class TileData {
+		public bool isMine;
+		public int tileCount;
+
+		public TileData() {
+			isMine = false;
+			tileCount = -1;
 		}
 	}
 }
